c1a877154463aaef4bdc5e2a4bc9b150762eeaa3
Mathias Gatti <mathigatti@gmail.com>
2020-10-02 21:15:54 -0300

    Update harmonizer.py

--- harmonizer.py
+++ harmonizer.py
@@ -7,6 +7,11 @@ notes = {}
 def run_process(command):
     return subprocess.Popen(command, shell = True)
 
+def stop_notes(key):
+    global notes
+    run_process("pkill sox")
+    notes = {}
+    
 def play_note(key):
     global notes
     try:
@@ -18,13 +23,8 @@ def play_note(key):
         command = f"sox --buffer 256 -c 1 -r 48000 -t alsa default -t alsa default pitch {note*100} &> /dev/null"
         notes[note] = run_process(command)
 
-def stop_notes(key):
-    global notes
-    run_process("pkill sox")
-    notes = {}
-
 # Collect events until released
 with keyboard.Listener(
         on_press=play_note,
-        on_release=stop_note) as listener:
+        on_release=stop_notes) as listener:
     listener.join()
