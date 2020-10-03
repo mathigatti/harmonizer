131252d63bef89f70aba23e913e275152b0c775a
Mathias Gatti <mathigatti@gmail.com>
2020-10-02 21:00:38 -0300

    Add files via upload

---
+++ harmonizer.py
@@ -0,0 +1,30 @@
+import subprocess
+
+from pynput import keyboard
+
+notes = {}
+
+def run_process(command):
+    return subprocess.Popen(command, shell = True)
+
+def play_note(key):
+    global notes
+    try:
+        note = int(key.char)
+    except:
+        stop_notes(None)
+        exit()
+    if not note in notes:
+        command = f"sox --buffer 256 -c 1 -r 48000 -t alsa default -t alsa default pitch {note*100} &> /dev/null"
+        notes[note] = run_process(command)
+
+def stop_notes(key):
+    global notes
+    run_process("pkill sox")
+    notes = {}
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=play_note,
+        on_release=stop_note) as listener:
+    listener.join()
